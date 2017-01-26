449a0ef162f8a65add7477d0d6346795a50f5d58
izaitere <ismahenez@gmail.com>
2017-01-26 15:16:15 -0500

    sesseion

--- views/sub_menu.php
+++ views/sub_menu.php
@@ -15,13 +15,14 @@
     <div class="container">
         <div class="row col-12"><!--row petit icons-->
             <div class="logo col-4 col-m-1">Logo</div>
-            <div class="user col-2 col-m-3">
-                <a href="compte.php"><img class="col-4" src="images/compte.svg" alt="compte"> Mon compte</a>
-            </div>
             <div class="panier col-2 col-m-3">
                 <a href="#"><img class="col-4" src="images/panier.svg" alt="panier">Mon panier</a>
             </div>
-            <a href="session.php">ouvrir une session</a>
+            <div class="user col-2 col-m-3">
+                <a href="session.php"><img class="col-4" src="images/compte.svg" alt="compte"> Mon compte</a>
+                <a href="session.php"><sub>Mot de passe oublié ?</sub></a>
+            </div>
+
 
         </div><!--fin row petit icons-->
     </div><!--fin conta  er-->
