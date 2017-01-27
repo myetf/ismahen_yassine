790b5a34ecee5610fbca4218c4dd1f75c310707c
izaitere <ismahenez@gmail.com>
2017-01-27 11:02:30 -0500

    ajout css et modif index

--- index.php
+++ index.php
@@ -48,45 +48,32 @@ include 'user_data.php';
 
             <!--   -------------------------------------------------------------  section main (new prod)  -->
 
-            <section class="container cat_articles row col-10">
-                <div class="row">
-                <article class="articles_items col-4 col-m-12">
-                    <ul>
-                        <?php foreach ($catalogue as $article){ ?>
-                            <li class="row art_ind col-4 col-m-12">
-                                <a href="#"><img class="img_art col-6" src="images/<?= $article['image'] ?>.jpeg" alt=""></a>
+        <section class="container cat_articles row col-9">
+            <div id="fig1" class="row">
+            <h1>SOLDES!</h1>
+            <img class="col-12" src="images/homme.jpeg" alt="">
+                <dl id="infos">
+                    <dt id="infos1">40%</dt>
+                    <dd id="infos1_exp"><a href="catalogue.php"><span>En savoir +</span></a></dd>
+                </dl>
+            </div>
+        </section>
 
-                                <div class="col-3 text_articles">
-                                    <h3><a href="<? $article['nom'] ?>">Découvrez en exclusivités</a></h3>
-                                    <p class="col-3"><? $article['Description'] ?></p>
-                                </div>
-                            </li>
-                      <?php  } ?>
+        <aside class="aside col-3 col-m-4">
+            <h4>L'actualité du moment</h4>
+            <p>Découvrez tous les évènements de cette semaine</p>
+        </aside>
+        <aside class="aside col-3 col-m-4">
+            <h4>L'actualité du moment</h4>
+            <p>Découvrez tous les évènements de cette semaine.enim ad minim veniamenim ad minim veniamenim ad minim
+                veniam
+                enim ad minim veniam enim ad minim veniam</p>
+        </aside>
+        <article class="row col-12 col-m-12">
+           <img class="col-8" src="images/hmenf.jpeg" alt="">
+        </article>
+    </div>
 
-                    </ul>
-                </article>
-
-                </div>
-            </section>
-                <div class="aside col-2 col-m-4">
-                    <h4>L'actualité du moment</h4>
-                    <p>Découvrez tous les évènements de cette semaine</p>
-                </div><!--fin produits vedette-->
-                <div class="aside col-2 col-m-4">
-                    <h4>L'actualité du moment</h4>
-                    <p>Découvrez tous les évènements de cette semaine</p>
-                </div>
-                <div class="aside col-2 col-m-4">
-                    <h4>L'actualité du moment</h4>
-                    <p>Découvrez tous les évènements de cette semaine</p>
-                </div>
-                <div class="aside col-2 col-m-4">
-                    <h4>L'actualité du moment</h4>
-                    <p>Découvrez tous les évènements de cette semaine</p>
-                </div>
-
-
-    </div><!--fin container main -->
 
 <?php
 require_once 'views/footer.php';
